a8d7babb5ec8918e279ba2ce951e002cf18f09e0
Erhan Ali Yükselen <erhanaly@gmail.com>
2023-03-30 11:08:48 +0300

    Add files via upload

---
+++ Models/Other.cs
@@ -0,0 +1,17 @@
+﻿namespace WebShopMVC.Models
+{
+    public static class Other
+    {
+        public const string Role_Person = "Person";
+        public const string Role_Admin = "Admin";
+        public const string Role_User = "User";
+        public const string ssShoppingCart = "Shopping Cart Session";
+
+        public const string Durum_Onaylandi = "Approved";
+        public const string Durum_Beklemede = "Pending";
+        public const string Durum_Kargoda = "Shipped";
+
+
+
+    }
+}
---
+++ Models/SessionExtensions.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
+
+namespace WebShopMVC.Models
+{
+    public static class SessionExtensions
+    {
+        //Kompleks nesneleri sessionda tutamıyoruz, buna çözüm olarak sessionda string değer taşıyabiliyoruz
+        //Kompleks nesneyi json formatına çevirip bu string veriyi sessiona yazıyoruz.
+        
+        public static void Set<T>(this ISession session, string key, T value)
+        {
+            session.SetString(key, JsonConvert.SerializeObject(value));
+        }
+
+        public static T? Get<T>(this ISession session, string key)
+        {
+            var value = session.GetString(key);
+            return value == null ? default(T) : JsonConvert.DeserializeObject<T>(value);
+        }
+    }
+}
