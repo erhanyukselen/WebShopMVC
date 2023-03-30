28de9a5cd5fd34f3ce86ea723c3de7fe9b4fc765
Erhan Ali Yükselen <erhanaly@gmail.com>
2023-03-30 11:08:28 +0300

    Add files via upload

---
+++ Migrations/20230314131157_AddPayment.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebShopMVC.Migrations
+{
+    public partial class AddPayment : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "CVC",
+                table: "OrderHeaders",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "CardName",
+                table: "OrderHeaders",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "CardNumber",
+                table: "OrderHeaders",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ExprationMonth",
+                table: "OrderHeaders",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ExprationYear",
+                table: "OrderHeaders",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CVC",
+                table: "OrderHeaders");
+
+            migrationBuilder.DropColumn(
+                name: "CardName",
+                table: "OrderHeaders");
+
+            migrationBuilder.DropColumn(
+                name: "CardNumber",
+                table: "OrderHeaders");
+
+            migrationBuilder.DropColumn(
+                name: "ExprationMonth",
+                table: "OrderHeaders");
+
+            migrationBuilder.DropColumn(
+                name: "ExprationYear",
+                table: "OrderHeaders");
+        }
+    }
+}
