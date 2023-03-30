fd0a24237420ff227f2c360dd22d325cf0940f79
Erhan Ali Yükselen <erhanaly@gmail.com>
2023-03-30 11:09:26 +0300

    Add files via upload

---
+++ ViewComponents/CategoryList.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using WebShopMVC.Data;
+
+namespace WebShopMVC.ViewComponents
+{
+    public class CategoryList:ViewComponent
+    {
+        private readonly ApplicationDbContext _db;
+        public CategoryList(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IViewComponentResult Invoke()
+
+        {
+            var category = _db.Categories.ToList();
+            return View(category); 
+        }
+    }
+}
